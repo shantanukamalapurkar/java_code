ff104d5db301539e5d4993bf5cb0708f0cb7bde6
Shantanu <shantanu.kamalapurkar@gmail.com>
2020-03-05 07:47:54 -0500

    addnumber file committed

--- src/main/java/addnumbers.java
+++ src/main/java/addnumbers.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class addnumbers {
+
+    void addmynumber(int x,int y){
+        System.out.println("addition is :"+ x+y);
+    }
+    public static void main(String args[]){
+        addnumbers add1 = new addnumbers();
+        add1.addmynumber(10,20);
+
+    }
 }
