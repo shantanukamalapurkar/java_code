ac84e590a011d969be37e67cc7c9aadade55f397
Shantanu <shantanu.kamalapurkar@gmail.com>
2020-03-07 08:07:20 -0500

    added files

--- src/main/java/addnumbers.java
+++ src/main/java/addnumbers.java
@@ -1,11 +1,10 @@
 public class addnumbers {
 
     void addmynumber(int x,int y){
-        System.out.println("addition is :"+ x+y);
+
+        System.out.println("addition is :" + (x+y));
     }
-    public static void main(String args[]){
-        addnumbers add1 = new addnumbers();
-        add1.addmynumber(10,20);
+
 
     }
-}
+
--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -1,6 +1,8 @@
 public class helloworld {
     public static void main(String args[]){
         System.out.println("Hello World");
+        addnumbers add1 = new addnumbers();
+        add1.addmynumber(10,20);
         /*
         Added comment
          */
