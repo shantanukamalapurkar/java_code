9d63956a4d53b41c812324694bc099c926534781
Shantanu <shantanu.kamalapurkar@gmail.com>
2020-03-08 16:05:05 -0400

    multiplycommitted

---
+++ src/main/java/multiplynumber.java
@@ -0,0 +1,13 @@
+public class multiplynumber {
+
+    void multiplynumber(int x, int y)
+    {
+        if(x!=0 & y!=0){
+            System.out.println("Multiplication is :"+ (x*y));
+        }
+        else{
+            System.out.println("0");
+        }
+    }
+
+}
