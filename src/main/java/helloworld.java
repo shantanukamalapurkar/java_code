24c0c622710666b717f148aa72c6983f5bf0a776
Shantanu <shantanu.kamalapurkar@gmail.com>
2020-03-08 16:06:41 -0400

    added

--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -5,6 +5,8 @@ public class helloworld {
         add1.addmynumber(10,20);
         substractnumbers sub = new substractnumbers();
         sub.substractnumbers(20,30);
+        multiplynumber multi = new multiplynumber();
+        multi.multiplynumber(10,20);
         /*
         Added comment
          */
